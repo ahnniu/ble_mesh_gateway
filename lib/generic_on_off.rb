6bc80e874eab410c4df384766eaf918c42ccd0b6
Yongjia Niu <ahnniu@gmail.com>
2019-07-05 04:26:45 +0100

    Add a generic onoff model

---
+++ lib/generic_on_off.rb
@@ -0,0 +1,61 @@
+class GenericOnOffServer < Model
+  def initialize()
+
+    @elements = []
+  end
+
+  def id
+    0x1000
+  end
+
+  def process_message(source, dest, opcode, data)
+
+  end
+
+  def pub_set(element_address, address)
+    @elements.each do |element|
+      if element[:element_address] == element_address
+        element[:publish_address] = address
+        return
+      end
+    end
+    element = { element_address: element_address, publish_address: address, state: 0 }
+    @elements.push element
+  end
+
+end
+
+
+class GenericOnOffClient < Model
+  attr_reader :elements
+
+  def initialize()
+    @elements = []
+    @trans_id = 0
+  end
+
+  def id
+    0x1001
+  end
+
+  def pub_set(element_address, address)
+    @elements.each do |element|
+      if element[:element_address] == element_address
+        element[:address] = address
+        return
+      end
+    end
+    element = { element_address: element_address, publish_address: address }
+    @elements.push element
+  end
+
+  def on(address)
+
+  end
+
+  def off(address)
+
+  end
+
+
+end
